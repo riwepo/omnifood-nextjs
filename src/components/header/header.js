31e027eaa889de8994e9e8817427cb818529b20b
Richard Potter <richardpotter.au@gmail.com>
2024-01-30 15:54:25 +1000

    re-arrange and style nav bar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,23 +3,33 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <header>
-      <nav className="flex items-center justify-between h-40 px-16 py-0">
-        <Link href="/">
-          <Image
-            alt="Omnifood logo"
-            src="/omnifood-logo.png"
-            // image width tio heigh is 7.5
-            // we want height 2.2rem
-            width={263}
-            height={35}
-          />
-        </Link>
+    <header className="flex items-center justify-between h-40 px-16 py-0">
+      <Link href="/">
+        <Image
+          alt="Omnifood logo"
+          src="/omnifood-logo.png"
+          // image width tio heigh is 7.5
+          // we want height 2.2rem
+          width={263}
+          height={35}
+        />
+      </Link>
 
-        <Link href="/">How it works</Link>
-        <Link href="/">Meals</Link>
-        <Link href="/">Testimonials</Link>
-        <Link href="/">Try for free</Link>
+      <nav>
+        <ul className="flex items-center gap-20 list-none">
+          <li>
+            <Link href="/">How it works</Link>
+          </li>
+          <li>
+            <Link href="/">Meals</Link>
+          </li>
+          <li>
+            <Link href="/">Testimonials</Link>
+          </li>
+          <li>
+            <Link href="/">Try for free</Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
