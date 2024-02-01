b8a11866714932d38dc161ef610f820264eda347
Richard Potter <richardpotter.au@gmail.com>
2024-02-01 16:42:03 +1000

    implement nav button

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,13 +4,28 @@ import Image from "next/image";
 function StyledNavLink({ children, href }) {
   return (
     <Link
-      className="text-lg font-medium hover:text-burnt-orange active:text-burnt-orange"
+      className="text-lg font-medium hover:text-burnt-orange-200 active:text-burnt-orange-200"
       href={href}
     >
       {children}
     </Link>
   );
 }
+function ButtonNavLink({ children, href }) {
+  return (
+    <Link
+      className="px-6 py-3 text-lg font-medium text-white rounded-lg hover:bg-burnt-orange-200 active:bg-burnt-orange-200 bg-burnt-orange-100"
+      href={href}
+    >
+      {children}
+    </Link>
+  );
+}
+
+// padding: 1.2rem 2.4rem;
+//   border-radius: 9px;
+//   color: #fff;
+//   background-color: #e67e22;
 
 function Header() {
   return (
@@ -38,7 +53,10 @@ function Header() {
             <StyledNavLink href="#testimonials">Testimonials</StyledNavLink>
           </li>
           <li>
-            <StyledNavLink href="#pricing">Try it for free</StyledNavLink>
+            <StyledNavLink href="#testimonials">Pricing</StyledNavLink>
+          </li>
+          <li>
+            <ButtonNavLink href="#pricing">Try for free</ButtonNavLink>
           </li>
         </ul>
       </nav>
