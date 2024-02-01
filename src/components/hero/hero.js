7327282d910c18c5681a8f55d95616b8133f6b04
Richard Potter <richardpotter.au@gmail.com>
2024-02-02 08:57:24 +1000

    style hero div

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <section className="pt-12 pb-6 pl-0 pr-0 bg-off-white">
-      <div className="hero">
+      <div className="max-w-[80rem] mx-auto">
         <div className="hero-text-box">
           <h1 className="heading-primary">
             A healthy meal delivered to your door, every single day
@@ -49,56 +49,14 @@ function Hero() {
   );
 }
 
-// .section-hero {
-//   background-color: #fdf2e9;
-//   padding: 4.8rem 0 9.6rem 0;
-// }
-
 export default Hero;
 
-{
-  /* <section class="section-hero">
-        <div class="hero">
-          <div class="hero-text-box">
-            <h1 class="heading-primary">
-              A healthy meal delivered to your door, every single day
-            </h1>
-            <p class="hero-description">
-              The smart 365-days-per-year food subscription that will make you
-              eat healthy again. Tailored to your personal tastes and
-              nutritional needs.
-            </p>
-            <a href="#cta" class="btn btn--full margin-right-sm"
-              >Start eating well</a
-            >
-
-            <a href="#how" class="btn btn--outline">Learn more &darr;</a>
-            <div class="delivered-meals">
-              <div class="delivered-imgs">
-                <img src="img/customers/customer-1.jpg" alt="Customer photo" />
-                <img src="img/customers/customer-2.jpg" alt="Customer photo" />
-                <img src="img/customers/customer-3.jpg" alt="Customer photo" />
-                <img src="img/customers/customer-4.jpg" alt="Customer photo" />
-                <img src="img/customers/customer-5.jpg" alt="Customer photo" />
-                <img src="img/customers/customer-6.jpg" alt="Customer photo" />
-              </div>
-              <p class="delivered-text">
-                <span>250,000+</span> meals delivered last year!
-              </p>
-            </div>
-          </div>
-          <div class="hero-img-box">
-            <picture>
-              <source srcset="img/hero.webp" type="image/webp" />
-              <source srcset="img/hero-min.png" type="image/png" />
-
-              <img
-                src="img/hero-min.png"
-                class="hero-img"
-                alt="Woman enjoying food, meals in storage container, and food bowls on a table"
-              />
-            </picture>
-          </div>
-        </div>
-      </section> */
-}
+// .hero {
+//   max-width: 130rem;
+//   margin: 0 auto;
+//   padding: 0 3.2rem;
+//   display: grid;
+//   grid-template-columns: 1fr 1fr;
+//   gap: 9.6rem;
+//   align-items: center;
+// }
