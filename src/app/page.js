df74e90200ace17f5087740fa82f98932c6675ea
Richard Potter <richardpotter.au@gmail.com>
2024-02-01 16:47:07 +1000

    added hero section placeholder

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
 import Header from "../components/header";
+import Hero from "../components/hero";
 
 function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
 
 export default Home;
