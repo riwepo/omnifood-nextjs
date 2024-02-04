80ddcce9efa0f9f045f14357157b60f501f06935
Richard Potter <richardpotter.au@gmail.com>
2024-02-04 16:38:35 +1000

    added second logo and sizing

--- src/components/featured/featured-logos.js
+++ src/components/featured/featured-logos.js
@@ -1,14 +1,25 @@
 import Image from "next/image";
 
 function FeaturedLogos() {
+  const logoHeight = 32;
+  const techCrunchRatio = 562 / 80;
+  const businessInsiderRatio = 252 / 80;
+  const techCrunchWidth = logoHeight * techCrunchRatio;
+  const businessInsiderWidth = logoHeight * businessInsiderRatio;
   return (
-    <div className="flex">
+    <div className="flex justify-around">
       <Image
-        // 562 x 80
         src="/img/logos/techcrunch.png"
         alt="Techcrunch logo"
-        height="32"
-        width="224"
+        height={logoHeight}
+        width={techCrunchWidth}
+        className="opacity-50 brightness-0"
+      />
+      <Image
+        src="/img/logos/business-insider.png"
+        alt="Business Insider logo"
+        height={logoHeight}
+        width={businessInsiderWidth}
         className="opacity-50 brightness-0"
       />
     </div>
