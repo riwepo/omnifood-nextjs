b24ccdcccdc27153a45e015e39834edd31e4e3ed
Richard Potter <richardpotter.au@gmail.com>
2024-02-04 15:40:55 +1000

    added featured component placeholder

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "../components/header";
 import Hero from "../components/hero";
+import Featured from "../components/featured";
 
 function Home() {
   return (
     <>
       <Header />
       <Hero />
+      <Featured />
     </>
   );
 }
