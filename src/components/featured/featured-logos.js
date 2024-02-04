b6c1d4d0b0fd2929a07b3144b18fa793db68953d
Richard Potter <richardpotter.au@gmail.com>
2024-02-04 16:46:27 +1000

    added more logos

--- src/components/featured/featured-logos.js
+++ src/components/featured/featured-logos.js
@@ -4,8 +4,14 @@ function FeaturedLogos() {
   const logoHeight = 32;
   const techCrunchRatio = 562 / 80;
   const businessInsiderRatio = 252 / 80;
+  const theNewYorkTimesRatio = 609 / 80;
+  const forbesRatio = 306 / 80;
+  const usaTodayRatio = 428 / 80;
   const techCrunchWidth = logoHeight * techCrunchRatio;
   const businessInsiderWidth = logoHeight * businessInsiderRatio;
+  const theNewYorkTimesWidth = logoHeight * theNewYorkTimesRatio;
+  const forbesWidth = logoHeight * forbesRatio;
+  const usaTodayWidth = logoHeight * usaTodayRatio;
   return (
     <div className="flex justify-around">
       <Image
@@ -20,6 +26,27 @@ function FeaturedLogos() {
         alt="Business Insider logo"
         height={logoHeight}
         width={businessInsiderWidth}
+        className="opacity-50 brightness-0"
+      />
+      <Image
+        src="/img/logos/the-new-york-times.png"
+        alt="The New York Times logo"
+        height={logoHeight}
+        width={theNewYorkTimesWidth}
+        className="opacity-50 brightness-0"
+      />
+      <Image
+        src="/img/logos/forbes.png"
+        alt="Forbes logo"
+        height={logoHeight}
+        width={forbesWidth}
+        className="opacity-50 brightness-0"
+      />
+      <Image
+        src="/img/logos/usa-today.png"
+        alt="USA Today logo"
+        height={logoHeight}
+        width={usaTodayWidth}
         className="opacity-50 brightness-0"
       />
     </div>
