0d2b543d61f49df4da89c2d82965c22538dbdbee
Richard Potter <richardpotter.au@gmail.com>
2024-02-10 09:56:07 +1000

    added whites

--- src/components/hero/delivered-image.js
+++ src/components/hero/delivered-image.js
@@ -7,7 +7,7 @@ function DeliveredImage({ src, alt }) {
       alt={alt}
       width={48}
       height={48}
-      className="-mr-4 rounded-full border-2 border-off-white last:mr-0"
+      className="border-whites-100 -mr-4 rounded-full border-2 last:mr-0"
     ></Image>
   );
 }
