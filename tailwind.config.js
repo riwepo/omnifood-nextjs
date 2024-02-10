ec1e27d9a20b826e59875747fefec12aa14709bb
Richard Potter <richardpotter.au@gmail.com>
2024-02-10 12:36:26 +1000

    get rid background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,6 @@ module.exports = {
       greys: { 100: "#333", 200: "#555", 300: "#888", 400: "#ddd" },
     },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       fontFamily: {
         sans: ["Rubik", "sans-serif"],
       },
