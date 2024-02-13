cecd5867eebea97670a57d75c4dff156071086d7
Richard Potter <richardpotter.au@gmail.com>
2024-02-13 15:58:09 +1000

    icons

--- src/components/pricing/inclusions.js
+++ src/components/pricing/inclusions.js
@@ -1,58 +1,45 @@
 import Feature from "./feature";
 import InfiniteIcon from "../../../public/icons/features/infinite-outline.svg";
 import NutritionIcon from "../../../public/icons/features/nutrition-outline.svg";
+import LeafIcon from "../../../public/icons/features/leaf-outline.svg";
+import PauseIcon from "../../../public/icons/features/pause-circle-outline.svg";
 
 function Inclusions() {
-  const feature1 = {
-    icon: (
-      <InfiniteIcon className="infinite-icon h-8 w-8 text-burnt-orange-100" />
-    ),
-    title: "Never cook again!",
-    text: "Our subscriptions cover 365 days per year, even including major holidays.",
-  };
-  const feature2 = {
-    icon: (
-      <NutritionIcon className="inutrition-icon h-8 w-8 text-burnt-orange-100" />
-    ),
-    title: "Local and organic",
-    text: "Our cooks only use local, fresh, and organic products to prepare your meals.",
-  };
+  const features = [
+    {
+      index: 0,
+      icon: (
+        <InfiniteIcon className="infinite-icon h-8 w-8 text-burnt-orange-100" />
+      ),
+      title: "Never cook again!",
+      text: "Our subscriptions cover 365 days per year, even including major holidays.",
+    },
+    {
+      index: 1,
+      icon: (
+        <NutritionIcon className="nutrition-icon h-8 w-8 text-burnt-orange-100" />
+      ),
+      title: "Local and organic",
+      text: "Our cooks only use local, fresh, and organic products to prepare your meals.",
+    },
+    {
+      index: 2,
+      icon: <LeafIcon className="leaf-icon h-8 w-8 text-burnt-orange-100" />,
+      title: "No waste",
+      text: "All our partners only use reusable containers to package all your meals.",
+    },
+    {
+      index: 3,
+      icon: <PauseIcon className="pause-icon h-8 w-8 text-burnt-orange-100" />,
+      title: "Pause anytime",
+      text: "Going on vacation? Just pause your subscription, and we refund unused days.",
+    },
+  ];
   return (
     <div className="grid--4-cols container mx-auto my-0 mb-12 grid max-w-[75rem] grid-cols-4">
-      <Feature featureData={feature1} />
-      <Feature featureData={feature2} />
-      {/* <div class="feature">
-        <ion-icon class="feature-icon" name="infinite-outline"></ion-icon>
-        <p class="feature-title">Never cook again!</p>
-        <p class="feature-text">
-          Our subscriptions cover 365 days per year, even including major
-          holidays.
-        </p>
-      </div>
-      <div class="feature">
-        <ion-icon class="feature-icon" name="nutrition-outline"></ion-icon>
-        <p class="feature-title">Local and organic</p>
-        <p class="feature-text">
-          Our cooks only use local, fresh, and organic products to prepare your
-          meals.
-        </p>
-      </div>
-      <div class="feature">
-        <ion-icon class="feature-icon" name="leaf-outline"></ion-icon>
-        <p class="feature-title">No waste</p>
-        <p class="feature-text">
-          All our partners only use reusable containers to package all your
-          meals.
-        </p>
-      </div>
-      <div class="feature">
-        <ion-icon class="feature-icon" name="pause-outline"></ion-icon>
-        <p class="feature-title">Pause anytime</p>
-        <p class="feature-text">
-          Going on vacation? Just pause your subscription, and we refund unused
-          days.
-        </p>
-      </div> */}
+      {features.map((feature) => {
+        return <Feature key={feature.index} featureData={feature} />;
+      })}
     </div>
   );
 }
