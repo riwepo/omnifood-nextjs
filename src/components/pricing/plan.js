b4cf555a90905c3ee64605ff94f575755e14983e
Richard Potter <richardpotter.au@gmail.com>
2024-02-13 10:29:24 +1000

    plan name

--- src/components/pricing/plan.js
+++ src/components/pricing/plan.js
@@ -1,8 +1,10 @@
 function Plan() {
   return (
     <div className="pricing-plan pricing-plan--starter w-3/4 justify-self-end rounded-xl border-2 border-whites-100  p-12">
-      <header class="plan-header">
-        <p class="plan-name">Starter</p>
+      <header className="plan-header mb-12 text-center">
+        <p className="plan-name mb-8 text-xl font-semibold uppercase tracking-[0.75px] text-burnt-orange-200">
+          Starter
+        </p>
         <p class="plan-price">
           <span>$</span>399
         </p>
@@ -46,4 +48,48 @@ export default Plan;
 //   justify-self: end;
 //   border: 2px solid #fdf2e9;
 //   padding: 4.6rem;
+// }
+
+// .plan-header {
+//   text-align: center;
+//   margin-bottom: 4.8rem;
+// }
+
+// .plan-name {
+//   color: #cf711f;
+//   font-weight: 600;
+//   font-size: 2rem;
+//   text-transform: uppercase;
+//   letter-spacing: 0.75;
+//   margin-bottom: 3.2rem;
+// }
+
+// .plan-price {
+//   font-size: 6.2rem;
+//   font-weight: 600;
+//   color: #333;
+//   margin-bottom: 1.6rem;
+// }
+
+// .plan-price span {
+//   font-size: 3rem;
+//   font-weight: 500;
+//   margin-right: 0.8rem;
+// }
+
+// .plan-text {
+//   font-size: 1.6rem;
+//   line-height: 1.6;
+//   color: #6f6f6f;
+// }
+
+// .plan-sing-up {
+//   text-align: center;
+//   margin-top: 4.8rem;
+// }
+
+// .plan-details {
+//   font-size: 1.6rem;
+//   line-height: 1.6;
+//   text-align: center;
 // }
