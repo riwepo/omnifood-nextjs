8a5daf8fbef377f07400cec0073d1a275aea40cd
Richard Potter <richardpotter.au@gmail.com>
2024-02-13 15:48:42 +1000

    features

--- src/components/pricing/feature.js
+++ src/components/pricing/feature.js
@@ -1,9 +1,9 @@
 function Feature({ featureData }) {
   return (
     <div class="feature">
-      {featureData.icon}
-      <ion-icon class="feature-icon" name="infinite-outline"></ion-icon>
-
+      <div className="mb-4 h-16 w-16 rounded-full bg-whites-100 p-4">
+        {featureData.icon}
+      </div>
       <p class="feature-title">{featureData.title}</p>
       <p class="feature-title">Never cook again!</p>
       <p class="feature-text">{featureData.text}</p>
@@ -16,3 +16,25 @@ function Feature({ featureData }) {
 }
 
 export default Feature;
+
+// .feature-icon {
+//     color: #e67e22;
+//     height: 3.2rem;
+//     width: 3.2rem;
+//     background-color: #fdf2e9;
+//     margin-bottom: 3.2rem;
+//     padding: 1.6rem;
+//     border-radius: 50%;
+//   }
+
+//   .feature-title {
+//     font-size: 2.4rem;
+//     color: #333;
+//     font-weight: 700;
+//     margin-bottom: 1.6rem;
+//   }
+
+//   .feature-text {
+//     font-size: 1.8rem;
+//     line-height: 1.8;
+//   }
--- src/components/pricing/inclusions.js
+++ src/components/pricing/inclusions.js
@@ -1,14 +1,27 @@
 import Feature from "./feature";
+import InfiniteIcon from "../../../public/icons/features/infinite-outline.svg";
+import NutritionIcon from "../../../public/icons/features/nutrition-outline.svg";
 
 function Inclusions() {
   const feature1 = {
+    icon: (
+      <InfiniteIcon className="infinite-icon h-8 w-8 text-burnt-orange-100" />
+    ),
     title: "Never cook again!",
     text: "Our subscriptions cover 365 days per year, even including major holidays.",
+  };
+  const feature2 = {
+    icon: (
+      <NutritionIcon className="inutrition-icon h-8 w-8 text-burnt-orange-100" />
+    ),
+    title: "Local and organic",
+    text: "Our cooks only use local, fresh, and organic products to prepare your meals.",
   };
   return (
     <div className="grid--4-cols container mx-auto my-0 mb-12 grid max-w-[75rem] grid-cols-4">
       <Feature featureData={feature1} />
-      <div class="feature">
+      <Feature featureData={feature2} />
+      {/* <div class="feature">
         <ion-icon class="feature-icon" name="infinite-outline"></ion-icon>
         <p class="feature-title">Never cook again!</p>
         <p class="feature-text">
@@ -39,9 +52,19 @@ function Inclusions() {
           Going on vacation? Just pause your subscription, and we refund unused
           days.
         </p>
-      </div>
+      </div> */}
     </div>
   );
 }
 
 export default Inclusions;
+
+// .feature-icon {
+//     color: #e67e22;
+//     height: 3.2rem;
+//     width: 3.2rem;
+//     background-color: #fdf2e9;
+//     margin-bottom: 3.2rem;
+//     padding: 1.6rem;
+//     border-radius: 50%;
+//   }
