a86c63d32b2cb89233e6617de28148372126ca07
Richard Potter <richardpotter.au@gmail.com>
2024-02-14 08:11:11 +1000

    mb

--- src/components/pricing/pricing.js
+++ src/components/pricing/pricing.js
@@ -14,7 +14,7 @@ function Pricing() {
       </div>
       <Plans />
       <div className="container mx-auto my-0 grid max-w-[75rem] px-8 py-0">
-        <aside className="plan-details text-center text-base leading-normal">
+        <aside className="plan-details mb-24 text-center text-base leading-normal">
           Prices include all applicable taxes. You can cancel at any time. Both
           plans include the following:
         </aside>
