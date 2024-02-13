d112467f8a60c3ba70c0804c32bf566c36627116
Richard Potter <richardpotter.au@gmail.com>
2024-02-13 10:37:21 +1000

    added list component

---
+++ src/components/ui/list.js
@@ -0,0 +1,12 @@
+function List(children) {
+  return <ul className="flex list-none flex-col gap-4">{children}</ul>;
+}
+
+export default List;
+
+// .list {
+//     list-style: none;
+//     display: flex;
+//     flex-direction: column;
+//     gap: 1.6rem;
+//   }
