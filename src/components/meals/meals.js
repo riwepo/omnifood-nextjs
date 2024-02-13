453b86f7dd6395b4b064a7ca5861cb3df35befe3
Richard Potter <richardpotter.au@gmail.com>
2024-02-13 10:01:41 +1000

    typo

--- src/components/meals/meals.js
+++ src/components/meals/meals.js
@@ -47,7 +47,7 @@ function Meals() {
         <Diets />
       </div>
 
-      <div class="all-recipes container m-auto max-w-[75rem] px-8 py-0 text-center text-lg ">
+      <div className="all-recipes container m-auto max-w-[75rem] px-8 py-0 text-center text-lg ">
         <a
           href="#"
           className="link inline-block border-b-[1px] border-current pb-[2px] text-burnt-orange-100 transition-all duration-300 hover:border-none hover:text-burnt-orange-200"
