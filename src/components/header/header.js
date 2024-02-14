2832aefbdd517b9976dedcf6357d95ce7eadd4d0
Richard Potter <richardpotter.au@gmail.com>
2024-02-15 09:36:47 +1000

    relative

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,13 +3,12 @@ import Image from "next/image";
 
 import Nav from "./Nav";
 
-
 function Header() {
   const imgScale = 0.135;
   const imgWidth = 1212 * imgScale;
   const imgHeight = 163 * imgScale;
   return (
-    <header className="flex h-24 items-center justify-between bg-whites-100 px-10 py-0">
+    <header className="relative flex h-24 items-center justify-between bg-whites-100 px-10 py-0">
       <Link href="/">
         <Image
           alt="Omnifood logo"
