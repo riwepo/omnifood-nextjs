a4bf43e00f57f08c8f2a99dc2b4af878203bec3a
Richard Potter <richardpotter.au@gmail.com>
2024-02-15 09:33:22 +1000

    refactor

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,69 +1,24 @@
 import Link from "next/link";
 import Image from "next/image";
 
-import { Button1 } from "../ui/buttons";
+import Nav from "./Nav";
 
-function StyledNavLink({ children, href }) {
-  return (
-    <Link
-      className="text-lg font-medium hover:text-burnt-orange-200 active:text-burnt-orange-200"
-      href={href}
-    >
-      {children}
-    </Link>
-  );
-}
-// function ButtonNavLink({ children, href }) {
-//   return (
-//     <Link
-//       className="rounded-lg bg-burnt-orange-100 px-6 py-3 text-lg font-medium text-white hover:bg-burnt-orange-200 active:bg-burnt-orange-200"
-//       href={href}
-//     >
-//       {children}
-//     </Link>
-//   );
-// }
-
-// padding: 1.2rem 2.4rem;
-//   border-radius: 9px;
-//   color: #fff;
-//   background-color: #e67e22;
 
 function Header() {
+  const imgScale = 0.135;
+  const imgWidth = 1212 * imgScale;
+  const imgHeight = 163 * imgScale;
   return (
     <header className="flex h-24 items-center justify-between bg-whites-100 px-10 py-0">
       <Link href="/">
         <Image
           alt="Omnifood logo"
           src="/img/omnifood-logo.png"
-          // image width to heigh is 7.5
-          // we want height 2.2rem
-          width={165}
-          height={22}
+          width={imgWidth}
+          height={imgHeight}
         />
       </Link>
-
-      <nav>
-        <ul className="flex list-none items-center gap-12">
-          <li>
-            <StyledNavLink href="#how">How it works</StyledNavLink>
-          </li>
-          <li>
-            <StyledNavLink href="#meals">Meals</StyledNavLink>
-          </li>
-          <li>
-            <StyledNavLink href="#testimonials">Testimonials</StyledNavLink>
-          </li>
-          <li>
-            <StyledNavLink href="#testimonials">Pricing</StyledNavLink>
-          </li>
-          <li>
-            <Button1 href="#pricing" extraClasses="px-4 py-2 text-lg">
-              Try for free
-            </Button1>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
     </header>
   );
 }
---
+++ src/components/header/nav.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+
+import { Button1 } from "../ui/buttons";
+
+function Nav() {
+  const navLinkData = [
+    { id: 1, href: "#how", name: "How it works" },
+    { id: 2, href: "#meals", name: "Meals" },
+    { id: 3, href: "#testimonials", name: "Testimonials" },
+    { id: 4, href: "#pricing", name: "Pricing" },
+  ];
+  return (
+    <nav>
+      <ul className="flex list-none items-center gap-12">
+        {navLinkData.map((link) => {
+          return (
+            <li key={link.id}>
+              <Link
+                className="text-lg font-medium hover:text-burnt-orange-200 active:text-burnt-orange-200"
+                href={link.href}
+              >
+                {link.name}
+              </Link>
+            </li>
+          );
+        })}
+        <li>
+          <Button1 href="#pricing" extraClasses="px-4 py-2 text-lg">
+            Try for free
+          </Button1>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
