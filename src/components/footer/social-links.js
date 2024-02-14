9b8c8878c98618e832d185893a6a622f1669fe52
Richard Potter <richardpotter.au@gmail.com>
2024-02-14 16:15:48 +1000

    links

--- src/components/footer/social-links.js
+++ src/components/footer/social-links.js
@@ -1,4 +1,6 @@
 import InstagramLogo from "../../../public/icons/social/logo-instagram.svg";
+import FacebookLogo from "../../../public/icons/social/logo-facebook.svg";
+import TwitterLogo from "../../../public/icons/social/logo-twitter.svg";
 
 function SocialLinks() {
   return (
@@ -6,20 +8,25 @@ function SocialLinks() {
       <li>
         <a className="footer-link" href="#">
           <InstagramLogo
-            className="social-icon h-8 w-8"
+            className="social-icon text-greys-700 h-8 w-8 text-base transition-all duration-300 hover:text-greys-200 active:text-greys-200"
             name="logo-instagram"
           />
-          <ion-icon className="social-icon" name="logo-instagram"></ion-icon>
         </a>
       </li>
       <li>
         <a className="footer-link" href="#">
-          <ion-icon className="social-icon" name="logo-facebook"></ion-icon>
+          <FacebookLogo
+            className="social-icon text-greys-700 h-8 w-8 text-base transition-all duration-300 hover:text-greys-200 active:text-greys-200"
+            name="logo-facebook"
+          />
         </a>
       </li>
       <li>
         <a className="footer-link" href="#">
-          <ion-icon className="social-icon" name="logo-twitter"></ion-icon>
+          <TwitterLogo
+            className="social-icon text-greys-700 h-8 w-8 text-base transition-all duration-300 hover:text-greys-200 active:text-greys-200"
+            name="logo-twitter"
+          />
         </a>
       </li>
     </ul>
@@ -30,6 +37,19 @@ function SocialLinks() {
 //   list-style: none;
 //   display: flex;
 //   gap: 2.4rem;
+// }
+
+// .footer-link:link,
+// .footer-link:visited {
+//   text-decoration: none;
+//   font-size: 1.6rem;
+//   color: #767676;
+//   transition: all 0.3s;
+// }
+
+// .footer-link:hover,
+// .footer-link:active {
+//   color: #555;
 // }
 
 export default SocialLinks;
