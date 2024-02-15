bd2ad4fed85db10713ed02473f4d9daa933a68bf
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 08:05:59 +1000

    gallery grid columns responsive

--- src/components/testimonials/gallery.js
+++ src/components/testimonials/gallery.js
@@ -3,7 +3,7 @@ import GalleryItem from "./gallery-item";
 function Gallery() {
   const galleryIndexes = Array.from({ length: 12 }, (v, k) => k + 1);
   return (
-    <div className="gallery grid grid-cols-3 gap-x-4 gap-y-4 p-4">
+    <div className="gallery grid grid-cols-4 gap-x-4 gap-y-4 p-4 md:grid-cols-6 xl:grid-cols-2 2xl:grid-cols-3">
       {galleryIndexes.map((index) => {
         return <GalleryItem key={index} index={index} />;
       })}
