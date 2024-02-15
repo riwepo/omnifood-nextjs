cca3dd359938815e74ba3d32dec16c0bee10f207
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 09:34:25 +1000

    hero cols responsive

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,7 +6,8 @@ function Hero() {
   return (
     <section className="relative bg-whites-100 pb-6 pl-0 pr-0 pt-12">
       <div
-        className="hero mx-auto mb-8 grid max-w-[75rem] grid-cols-2 items-center gap-16 px-2 lg:gap-12 xl:gap-24 2xl:max-w-[80rem]
+        className="hero mx-auto mb-8 grid max-w-[75rem] grid-cols-1 items-center gap-16 px-2 lg:grid-cols-2 lg:gap-12 xl:gap-24 2xl:max-w-[80rem]
+
 "
       >
         <HeroText />
