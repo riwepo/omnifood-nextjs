e62c3661e0efd1d2e219cd289f79a41d3af8b7cd
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 09:29:24 +1000

    testimonials container padding responsive

--- src/components/testimonials/testimonial-people-container.js
+++ src/components/testimonials/testimonial-people-container.js
@@ -4,7 +4,7 @@ import TestimonialPeople from "./testimonial-people";
 
 function TestimonialPeopleContainer() {
   return (
-    <div className="testimonials-container p-24">
+    <div className="testimonials-container px-8 py-24 xl:px-24 xl:py-24">
       <Subheading>Testimonials</Subheading>
       <HeadingSecondary>Once you try it, you can't go back</HeadingSecondary>
       <TestimonialPeople />
