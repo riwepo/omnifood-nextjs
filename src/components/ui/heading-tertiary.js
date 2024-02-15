60edba9745caf736ef8e2963c52f66476db9d142
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 09:08:14 +1000

    heading tertiary font size responsive

--- src/components/ui/heading-tertiary.js
+++ src/components/ui/heading-tertiary.js
@@ -1,37 +1,9 @@
 function HeadingTertiary({ children }) {
   return (
-    <h3 className="mb-8 text-[1.88rem] font-bold leading-5 tracking-[-0.5px] text-greys-100">
+    <h3 className="mb-8 text-[1.5rem] font-bold leading-5 tracking-[-0.5px] text-greys-100 xl:text-[1.88rem]">
       {children}
     </h3>
   );
 }
 
 export default HeadingTertiary;
-
-// .heading-primary,
-// .heading-secondary,
-// .heading-tertiary {
-//   font-weight: 700;
-//   color: #333;
-//   /* color: #45260a; */
-//   /* color: #343a40; */
-//   letter-spacing: -0.5px;
-// }
-
-// .heading-primary {
-//   font-size: 5.2rem;
-//   line-height: 1.05;
-//   margin-bottom: 3.2rem;
-// }
-
-// .heading-secondary {
-//   font-size: 4.4rem;
-//   line-height: 1.2;
-//   margin-bottom: 9.6rem;
-// }
-
-// .heading-tertiary {
-//   font-size: 3rem;
-//   line-height: 1.2;
-//   margin-bottom: 3.2rem;
-// }
