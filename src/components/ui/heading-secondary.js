1d7646497fb3e3e14b8b95767c8068ad3423a2b3
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 08:57:58 +1000

    heading secondary font size reactive

--- src/components/ui/heading-secondary.js
+++ src/components/ui/heading-secondary.js
@@ -2,38 +2,10 @@ import { cn } from "../../lib/utils";
 
 function HeadingSecondary({ children, extraClasses }) {
   const classes = cn(
-    "mb-24 text-left text-[2.75rem] font-bold leading-5 tracking-[-0.5px] text-greys-100",
+    "mb-24 text-left font-bold leading-5 tracking-[-0.5px] text-greys-100 text-[2.25rem] xl:text-[2.75rem]",
     extraClasses,
   );
   return <h2 className={classes}>{children}</h2>;
 }
 
 export default HeadingSecondary;
-
-// .heading-primary,
-// .heading-secondary,
-// .heading-tertiary {
-//   font-weight: 700;
-//   color: #333;
-//   /* color: #45260a; */
-//   /* color: #343a40; */
-//   letter-spacing: -0.5px;
-// }
-
-// .heading-primary {
-//   font-size: 5.2rem;
-//   line-height: 1.05;
-//   margin-bottom: 3.2rem;
-// }
-
-// .heading-secondary {
-//   font-size: 4.4rem;
-//   line-height: 1.2;
-//   margin-bottom: 9.6rem;
-// }
-
-// .heading-tertiary {
-//   font-size: 3rem;
-//   line-height: 1.2;
-//   margin-bottom: 3.2rem;
-// }
