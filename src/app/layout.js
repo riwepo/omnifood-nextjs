16270ac46dac92a15e0336591213f8fdbcbad20f
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 08:44:28 +1000

    make root text size responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="text-[0.8rem] lg:text-[0.9rem] 2xl:text-[1rem]">
       <body className={rubik.className}>{children}</body>
     </html>
   );
