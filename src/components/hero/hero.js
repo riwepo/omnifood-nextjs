d06d49cd7ee7d9a2aef3f9dfd99a4ffc90be2d32
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 09:23:47 +1000

    hero gap responsive

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -5,7 +5,10 @@ import HeroPicture from "./hero-picture";
 function Hero() {
   return (
     <section className="relative bg-whites-100 pb-6 pl-0 pr-0 pt-12">
-      <div className="hero mx-auto mb-8 grid max-w-[75rem] grid-cols-2 items-center gap-24 px-2 2xl:max-w-[80rem]">
+      <div
+        className="hero mx-auto mb-8 grid max-w-[75rem] grid-cols-2 items-center gap-16 px-2 lg:gap-12 xl:gap-24 2xl:max-w-[80rem]
+"
+      >
         <HeroText />
         <HeroPicture />
       </div>
