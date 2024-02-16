4da5f4e331fe1422d798c799a7b21072c36d2197
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 12:22:52 +1000

    static import logos, adjust height with style

--- src/components/featured/featured-logos.js
+++ src/components/featured/featured-logos.js
@@ -1,27 +1,36 @@
 import Image from "next/image";
 
+import techCrunchLogo from "@/public/img/logos/techcrunch.png";
+import businessInsiderLogo from "@/public/img/logos/business-insider.png";
+import theNewYorkTimesLogo from "@/public/img/logos/the-new-york-times.png";
+import forbesLogo from "@/public/img/logos/forbes.png";
+import usaTodayLogo from "@/public/img/logos/usa-today.png";
+
 function FeaturedLogos() {
-  const displayHeight = 32;
-  // all images have the same height, but verying widths
-  const imgHeight = 80;
-  const imageData = [
-    { index: 1, name: "techcrunch", width: 562 },
-    { index: 2, name: "business-insider", width: 252 },
-    { index: 3, name: "the-new-york-times", width: 609 },
-    { index: 4, name: "forbes", width: 306 },
-    { index: 5, name: "usa-today", width: 428 },
+  const logoData = [
+    { index: 1, logo: techCrunchLogo, name: "techcrunch" },
+    {
+      index: 2,
+      logo: businessInsiderLogo,
+      name: "business-insider",
+    },
+    {
+      index: 3,
+      logo: theNewYorkTimesLogo,
+      name: "the-new-york-times",
+    },
+    { index: 4, logo: forbesLogo, name: "forbes" },
+    { index: 5, logo: usaTodayLogo, name: "usa-today" },
   ];
   return (
     <ul className="flex justify-around">
-      {imageData.map((img) => {
+      {logoData.map((img) => {
         return (
           <li key={img.index}>
             <Image
-              src={`/img/logos/${img.name}.png`}
+              src={img.logo}
               alt={`${img.name} logo`}
-              height={displayHeight}
-              width={(displayHeight * img.width) / imgHeight}
-              className="opacity-50 brightness-0"
+              className="h-3 w-full opacity-50 brightness-0 md:h-6 lg:h-8"
             />
           </li>
         );
@@ -31,9 +40,3 @@ function FeaturedLogos() {
 }
 
 export default FeaturedLogos;
-
-// .logos img {
-//   height: 3.2rem;
-//   filter: brightness(0);
-//   opacity: 50%;
-// }
