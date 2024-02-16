204a3966ed550607fbc1e6cd03958a0ba78f6137
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 17:06:17 +1000

    grid colums responsive

--- src/components/call-to-action/call-to-action.js
+++ src/components/call-to-action/call-to-action.js
@@ -9,7 +9,10 @@ function CallToAction() {
       className="section-cta bg-white px-0 pb-32 pt-12"
     >
       <div className="container m-auto max-w-[75rem] px-8 py-0 text-center">
-        <div className="cta grid grid-cols-[2fr_1fr] rounded-xl bg-gradient-to-br from-burnt-orange-400 to-burnt-orange-100 shadow-xl shadow-black/25">
+        <div
+          className="cta grid grid-cols-1 overflow-hidden rounded-xl
+ bg-gradient-to-br from-burnt-orange-400 to-burnt-orange-100 shadow-xl shadow-black/25 md:grid-cols-[3fr_2fr] lg:grid-cols-[2fr_1fr]"
+        >
           <div className="cta-text-box pb-16 pl-16 pr-16 pt-12 text-burnt-orange-300">
             <HeadingSecondary extraClasses="text-inherit mb-8">
               Get your first meal for free!
