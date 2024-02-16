d0d86e5bed9754160645c19c0d81f8ec151e019e
Richard Potter <richardpotter.au@gmail.com>
2024-02-17 08:02:42 +1000

    height responsive

--- src/components/call-to-action/picture.js
+++ src/components/call-to-action/picture.js
@@ -1,7 +1,7 @@
 function Picture() {
   return (
     <div
-      className="row-start-1 bg-[linear-gradient(to_right_bottom,rgba(235,151,78,0.35),rgba(230,125,34,0.35)),url('/img/eating.jpg')] bg-cover bg-center"
+      className="row-start-1 h-80 bg-[linear-gradient(to_right_bottom,rgba(235,151,78,0.35),rgba(230,125,34,0.35)),url('/img/eating.jpg')] bg-cover bg-center md:h-auto"
       role="img"
       aria-label="Woman enjoying food"
     ></div>
