09e467bab89f391253fa385790af80773c5817c9
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 16:15:21 +1000

    static images

--- src/components/testimonials/testimonial-people.js
+++ src/components/testimonials/testimonial-people.js
@@ -1,43 +1,44 @@
 import TestimonialPerson from "./testimonial-person";
 
+import daveImg from "@/public/img/customers/testimonials/dave.jpg";
+import benImg from "@/public/img/customers/testimonials/ben.jpg";
+import steveImg from "@/public/img/customers/testimonials/steve.jpg";
+import hannahImg from "@/public/img/customers/testimonials/hannah.jpg";
+
 function TestimonialPeople() {
   const personData = [
     {
+      key: 1,
       name: "Dave Bryson",
-      imgSrc: "/img/customers/testimonials/dave.jpg",
+      img: daveImg,
       text: "Inexpensive, healthy and great-tasting meals, without even having to order manually! It feels truly magical.",
     },
     {
+      key: 2,
       name: "Ben Hadley",
-      imgSrc: "/img/customers/testimonials/ben.jpg",
+      img: benImg,
       text: "The AI algorithm is crazy good, it chooses the right meals for me every time. It's amazing not to worry about food anymore!",
     },
     {
+      key: 3,
       name: "Steve Miller",
-      imgSrc: "/img/customers/testimonials/steve.jpg",
+      img: steveImg,
       text: "Omnifood is a life saver! I just started a company, so there's no time for cooking. I couldn't live without my daily meals now!",
     },
     {
+      key: 4,
       name: "Hannah Smith",
-      imgSrc: "/img/customers/testimonials/hannah.jpg",
+      img: hannahImg,
       text: "I got Omnifood for the whole family, and it frees up so much time! Plus, everything is organic and vegan and without plastic.",
     },
   ];
   return (
     <div className="testimonials grid grid-cols-2 gap-x-20 gap-y-12 ">
-      <TestimonialPerson personData={personData[0]} />
-      <TestimonialPerson personData={personData[1]} />
-      <TestimonialPerson personData={personData[2]} />
-      <TestimonialPerson personData={personData[3]} />
+      {personData.map((person) => {
+        return <TestimonialPerson key={person.key} personData={person} />;
+      })}
     </div>
   );
 }
 
 export default TestimonialPeople;
-
-// .testimonials {
-//   display: grid;
-//   grid-template-columns: 1fr 1fr;
-//   row-gap: 4.8rem;
-//   column-gap: 8rem;
-// }
--- src/components/testimonials/testimonial-person.js
+++ src/components/testimonials/testimonial-person.js
@@ -1,21 +1,17 @@
 import Image from "next/image";
 
 function TestimonialPerson({ personData }) {
-  const imgWidth = 128 * 1;
-  const imgHeight = 128 * 1;
   return (
     <figure className="testimonial">
       <Image
-        width={imgHeight}
-        height={imgWidth}
         className="testimonial-img mb-5 rounded-full"
         alt={`Photo of customer ${personData.name}`}
-        src={personData.imgSrc}
+        src={personData.img}
       />
       <blockquote className="testimonial-text mb-4 text-lg leading-relaxed">
         {personData.text}
       </blockquote>
-      <p className="testimonial-name text-greys-500 text-base">
+      <p className="testimonial-name text-base text-greys-500">
         &mdash; {personData.name}
       </p>
     </figure>
@@ -23,20 +19,3 @@ function TestimonialPerson({ personData }) {
 }
 
 export default TestimonialPerson;
-
-// .testimonial-img {
-//   width: 6.4rem;
-//   border-radius: 50%;
-//   margin-bottom: 1.2rem;
-// }
-
-// .testimonial-text {
-//   font-size: 1.8rem;
-//   line-height: 1.8;
-//   margin-bottom: 1.6rem;
-// }
-
-// .testimonial-name {
-//   font-size: 1.6rem;
-//   color: #6f6f6f;
-// }
