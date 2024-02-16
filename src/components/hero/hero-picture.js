e059d348f5c5eb4b27c632a316d2a94fb40d7343
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 10:58:25 +1000

    use next Image

--- src/components/hero/hero-picture.js
+++ src/components/hero/hero-picture.js
@@ -1,16 +1,16 @@
+import Image from "next/image";
+
 function HeroPicture() {
+  const imgWidth = 1200;
+  const imgHeight = 1184;
   return (
     <div className="col-auto text-center lg:text-left">
-      <picture>
-        <source srcSet="img/hero.webp" type="image/webp" />
-        <source srcSet="img/hero-min.png" type="image/png" />
-
-        <img
-          src="img/hero-min.png"
-          className="hero-img"
-          alt="Woman enjoying food, meals in storage container, and food bowls on a table"
-        />
-      </picture>
+      <Image
+        src="/img/hero.webp"
+        alt="Woman enjoying food, meals in storage container, and food bowls on a table"
+        width={imgWidth}
+        height={imgHeight}
+      />
     </div>
   );
 }
