f9e74bd4a4ab186471b7a6b8018eb7dcdb85e1ff
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 10:18:51 +1000

    hero padding responsive

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -6,7 +6,7 @@ function Hero() {
   return (
     <section className="relative bg-whites-100 pb-6 pl-0 pr-0 pt-12">
       <div
-        className="hero mx-auto mb-8 grid max-w-[75rem] grid-cols-1 items-center gap-16 px-2 lg:grid-cols-2 lg:gap-12 xl:gap-24 2xl:max-w-[80rem]
+        className="hero mx-auto mb-8 grid max-w-[75rem] grid-cols-1 items-center gap-16 px-8 py-0 md:px-16 lg:grid-cols-2 lg:gap-12 lg:px-8 xl:gap-24 2xl:max-w-[80rem]
 
 "
       >
