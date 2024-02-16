65df941c8ea8f12e5a830a6ffc2f12b43e6dcb1a
Richard Potter <richardpotter.au@gmail.com>
2024-02-16 14:59:56 +1000

    use static

--- src/components/hero/delivered-images.js
+++ src/components/hero/delivered-images.js
@@ -1,16 +1,34 @@
-import DeliveredImage from "./delivered-image";
+import Image from "next/image";
 
+import customer1 from "@/public/img/customers/customer-1.jpg";
+import customer2 from "@/public/img/customers/customer-2.jpg";
+import customer3 from "@/public/img/customers/customer-3.jpg";
+import customer4 from "@/public/img/customers/customer-4.jpg";
+import customer5 from "@/public/img/customers/customer-5.jpg";
+
+const imageData = [
+  { index: 1, src: customer1 },
+  { index: 2, src: customer2 },
+  { index: 3, src: customer3 },
+  { index: 4, src: customer4 },
+  { index: 5, src: customer5 },
+];
+console.log(imageData);
 function DeliveredImages() {
   return (
     <div className="flex">
-      <DeliveredImage src="/img/customers/customer-1.jpg" alt="customer" />
-      <DeliveredImage src="/img/customers/customer-2.jpg" alt="customer" />
-      <DeliveredImage src="/img/customers/customer-3.jpg" alt="customer" />
-      <DeliveredImage src="/img/customers/customer-4.jpg" alt="customer" />
-      <DeliveredImage src="/img/customers/customer-5.jpg" alt="customer" />
-      <DeliveredImage src="/img/customers/customer-6.jpg" alt="customer" />
+      {imageData.map((img) => {
+        return (
+          <Image
+            key={img.index}
+            src={img.src}
+            alt="customer"
+            className="-mr-4 h-12 w-12 rounded-full border-2 border-whites-100 last:mr-0"
+          ></Image>
+        );
+      })}
     </div>
   );
- }
+}
 
 export default DeliveredImages;
