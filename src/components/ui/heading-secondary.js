52695d37c1fc12a9d1b723a3b1cc65faa107c7cb
Richard Potter <richardpotter.au@gmail.com>
2024-02-20 17:13:09 +1000

    reduce mb

--- src/components/ui/heading-secondary.js
+++ src/components/ui/heading-secondary.js
@@ -2,7 +2,7 @@ import { cn } from "@/lib/utils";
 
 function HeadingSecondary({ children, extraClasses }) {
   const classes = cn(
-    "text-left font-bold leading-5 tracking-[-0.5px] text-greys-100 text-[2.25rem] xl:text-[2.75rem] mb-12 lg:mb-24",
+    "text-left font-bold leading-5 tracking-[-0.5px] text-greys-100 text-[2.25rem] xl:text-[2.75rem] mb-12 lg:mb-12",
     extraClasses,
   );
   return <h2 className={classes}>{children}</h2>;
