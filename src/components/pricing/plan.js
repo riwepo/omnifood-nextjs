cbe5c3a786e6da1aaebdd4353b02cff3278cb30a
Richard Potter <richardpotter.au@gmail.com>
2024-02-20 17:01:43 +1000

    tweak layout

--- src/components/pricing/plan.js
+++ src/components/pricing/plan.js
@@ -1,10 +1,10 @@
-import PlanItem from "./planItem";
+import PlanItem from "./plan-item";
 import { Button1 } from "../ui/buttons";
 
 function Plan({ planData }) {
   let customClasses = "";
   if (planData.name === "Starter") {
-    customClasses = "justify-self-end border-2 border-whites-100";
+    customClasses = "border-2 border-whites-100";
   } else if (planData.name === "Complete") {
     customClasses = `
     bg-whites-100
@@ -29,7 +29,9 @@ function Plan({ planData }) {
       className={`
       pricing-plan
       pricing-plan--starter
-      w-3/4
+      mx-auto
+      max-w-[38rem]
+      self-center
       rounded-xl
       p-12
       ${customClasses}`}
@@ -65,94 +67,3 @@ function Plan({ planData }) {
 }
 
 export default Plan;
-
-// .pricing-plan {
-//   border-radius: 11px;
-
-//   width: 75%;
-// }
-
-// .pricing-plan--starter {
-//   justify-self: end;
-//   border: 2px solid #fdf2e9;
-//   padding: 4.6rem;
-// }
-
-// .plan-header {
-//   text-align: center;
-//   margin-bottom: 4.8rem;
-// }
-
-// .plan-name {
-//   color: #cf711f;
-//   font-weight: 600;
-//   font-size: 2rem;
-//   text-transform: uppercase;
-//   letter-spacing: 0.75;
-//   margin-bottom: 3.2rem;
-// }
-
-// .plan-price {
-//   font-size: 6.2rem;
-//   font-weight: 600;
-//   color: #333;
-//   margin-bottom: 1.6rem;
-// }
-
-// .plan-price span {
-//   font-size: 3rem;
-//   font-weight: 500;
-//   margin-right: 0.8rem;
-// }
-
-// .plan-text {
-//   font-size: 1.6rem;
-//   line-height: 1.6;
-//   color: #6f6f6f;
-// }
-
-// .plan-sing-up {
-//   text-align: center;
-//   margin-top: 4.8rem;
-// }
-
-// .plan-details {
-//   font-size: 1.6rem;
-//   line-height: 1.6;
-//   text-align: center;
-// }
-
-// .list {
-//   list-style: none;
-//   display: flex;
-//   flex-direction: column;
-//   gap: 1.6rem;
-// }
-
-// .pricing-plan--starter {
-//   justify-self: end;
-//   border: 2px solid #fdf2e9;
-//   padding: 4.6rem;
-// }
-
-// .pricing-plan--complete {
-//   background-color: #fdf2e9;
-//   padding: 4.8rem;
-//   position: relative;
-//   overflow: hidden;
-// }
-
-// .pricing-plan--complete::after {
-//   content: "Best value";
-//   position: absolute;
-//   top: 6%;
-//   right: -18%;
-
-//   text-transform: uppercase;
-//   font-size: 1.4rem;
-//   font-weight: 700;
-//   color: #333;
-//   background-color: #ffd43b;
-//   padding: 0.8rem 8rem;
-//   transform: rotate(45deg);
-// }
